e091d68649d21ff40a5b5e9134f0c0e0ce98f970
Letitia09 <74643959+Letitia09@users.noreply.github.com>
2022-06-05 15:34:59 +0530

    Create file.c

---
+++ Codes/file.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int fact(int n){
+   int fact=1;
+   while(n!=0){
+        fact=fact*n;
+        n-=1;
+    }
+    return fact;
+}
+int LNZD_FACTORIAL(int res){
+   while(res!=0){
+        if (res%10!=0){
+            return res%10;
+            break;
+        }
+        res=(int)res/10;
+    } 
+}
+int main()
+{
+    int n=5;
+    int res=fact(n);
+    printf("Last Non-Zero Digit of %d Factorial is %d",n,LNZD_FACTORIAL(res));
+
+    return 0;
+}
